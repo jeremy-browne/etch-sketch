41be02dbd79ea315b662f9afe17de71b419d1a4d
Jeremy Browne <jeremybrowne1991@hotmail.com>
2021-08-14 13:32:11 +1000

    fixed grid sizing issue

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
 const container = document.getElementById("container");
 const slider = document.getElementById("gridSize");
 const output = document.getElementById("gridValue");
+const resetButton = document.getElementById('reset');
+const maxSize = window.innerWidth / 8;
+
+function changeColor(e) {
+	e.target.style.backgroundColor = 'black';
+}
+
+function changeColor(e) {
+	e.target.style.backgroundColor = 'black';
+}
+
+function buildGrid(gridSize) {
+	container.style.setProperty('--grid-cols', gridSize);
+	container.style.setProperty('margin', 'auto');
+	container.style.setProperty('margin-top', '30px');
+	for (let i = 0; i < (gridSize * gridSize); i++) {
+		let element = document.createElement('div');
+		element.addEventListener('mouseover', changeColor);
+		container.appendChild(element).className = 'grid-item';
+	}
+}
+
+function resetGrid() {
+	container.innerHTML = '';
+	buildGrid(gridSize);
+}
+
 let gridSize = slider.value;
 output.innerHTML = gridSize;
 
@@ -10,29 +37,6 @@ slider.oninput = function() {
 	resetGrid(gridSize);
 }
 
-function changeColor(e) {
-	e.target.style.backgroundColor = 'black';
-}
-
-function setDivs(gridSize) {
-	container.style.setProperty("--grid-rows", gridSize);
-	container.style.setProperty("--grid-cols", gridSize);
-	for (let i = 0; i < gridSize * gridSize; i++) {
-		let elem = document.createElement("div");
-		// elem.innerText = i + 1;
-		elem.addEventListener('mouseover', changeColor);
-		container.appendChild(elem).className = "grid-item";
-	}
-}
-
-function resetGrid() {
-	container.innerHTML = '';
-	setDivs(gridSize);
-}
-
-//document.addEventListener("DOMContentLoaded", setDivs(16, 16));
-window.onload = (setDivs(gridSize));
-
-const resetButton = document.getElementById('reset');
-
 resetButton.onclick = () => resetGrid();
+
+window.onload = (buildGrid(gridSize, gridSize));
